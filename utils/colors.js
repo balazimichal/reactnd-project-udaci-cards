1baabe1259ee6eaa04e128eb12db34a71aa79dcb
Michal Balazi <balazi.michal@gmail.com>
2018-08-21 12:23:50 +0200

    feat - adding color scheme

---
+++ utils/colors.js
@@ -0,0 +1,8 @@
+export const white = "#fff";
+export const blue = "#02b3e4";
+export const black = "#1c262f";
+export const darkgrey = "#58646d";
+export const lightgrey = "#7d97ad";
+export const orange = "#ffae0c";
+export const green = "#15c26b";
+export const red = "#c7254e";
